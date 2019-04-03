a2d6b7d83a1bfd2677221c899f991940a0de9280
carlos-puerto-garcia <cpuertog@gmail.com>
2019-04-03 15:06:30 -0500

    modulo de pruebas unitarias

---
+++ pruebas_unitarias.py
@@ -0,0 +1,24 @@
+import Test_Carlos_PuertoG
+import unittest
+
+'''
+creamos los casos de prueba para las funciones
+y para eso debemos crear una clase
+'''
+
+class Test_ejemplo_suma (unittest.TestCase):
+    #Escribimos un metodo:
+    def test_suma (self):
+        result=Test_Carlos_PuertoG.suma(7,3)
+        self.assertEqual(result,10)
+
+#Despues de este mensaje ustedes deben integrar sus casos de prueba
+
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
+
