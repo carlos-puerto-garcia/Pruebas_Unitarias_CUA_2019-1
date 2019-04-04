2808bbfd378d8262575afd4c239f58d4f4c2e02a
vperez864 <vperez864@gmail.com>
2019-04-03 19:43:42 -0500

    Viviana Perez

---
+++ Test_Multiplica_Vperez.py
@@ -0,0 +1,13 @@
+def multiplicacion(x,y):
+        return x*y
+
+def isPrime(num):
+    if num < 1:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True     
